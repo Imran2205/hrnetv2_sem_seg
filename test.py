792ee605c24a36a98c4dde003cfc8c3123a422a0
imran2205 <imrankabir1996@gmail.com>
2025-03-18 12:58:56 -0400

    organizing folder structure

--- test.py
+++ test.py
@@ -11,10 +11,10 @@ import torch.backends.cudnn as cudnn
 import torch.optim
 from tensorboardX import SummaryWriter
 
-from networks.hrnet_v2.config import config_hrnet_v2 as config
-from networks.hrnet_v2.config import update_config_hrnet_v2 as update_config
 from core.criterion import CrossEntropy, OhemCrossEntropy
 from core.function import validate
+from networks.hrnet_v2.config import config_hrnet_v2 as config
+from networks.hrnet_v2.config import update_config_hrnet_v2 as update_config
 from networks.hrnet_v2.hrnet_v2_utils import create_logger, FullModel
 from networks.hrnet_v2.hrnet_v2_utils import get_imagenet_mean_std
 from dataloaders.uws.uws_full_dataloader import UWSDataLoader
